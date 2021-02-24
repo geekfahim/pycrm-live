53ccc2f6d11c4bbd0d4f24e24e38a92bac4dfa87
Fahim Ahmed <devfahimahmed@gmail.com>
2021-02-24 20:36:58 +0600

    settings.py import heroku

--- crm1/settings.py
+++ crm1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +29,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['geekfahim-crm.herokuapp.com','127.0.0.1']
 
 
-import django_heroku
 
 # Application definition
 
